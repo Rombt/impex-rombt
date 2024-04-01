966148e674c9f38296c1e899b8159679643a0926
Rombt <Rombt@gmail.com>
2024-04-01 11:34:12 +0300

    added classHorizontalMenu.js and tabs.js

--- app/src/assets/js/app.js
+++ app/src/assets/js/app.js
@@ -1,16 +1,14 @@
-import "./modules/functions.js";
-import "./modules/dynamic_adapt.js";
-import "./modules/popup.js";
+import './modules/functions.js';
+import './modules/dynamic_adapt.js';
+import './modules/popup.js';
 
-import "./modules/burger.js";
-import "./modules/spoiler.js";
-// import "./modules/tabs.js";   // the file lost
-import "./modules/arrowsInputNumberStyle.js";
+import './modules/spoiler.js';
+import './modules/tabs.js';
+import './modules/arrowsInputNumberStyle.js';
 
-
+import './modules/classHorizontalMenu.js';
 
 // let tl = gsap.timeline();
-
 
 // tl.to('.test-box__green', { duration: 2, x: 800, ease: 'elastic' });
 // tl.to('.test-box__green', { duration: 2, y: 200, ease: 'elastic' });
