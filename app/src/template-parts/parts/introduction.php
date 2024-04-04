a087c9936d80231b6fc95911244f15071d1c192e
Rombt <Rombt@gmail.com>
2024-04-04 10:48:17 +0300

    Introduction is ready

--- app/src/template-parts/parts/introduction.php
+++ app/src/template-parts/parts/introduction.php
@@ -5,27 +5,39 @@
             <article>
                <h2><?php echo rmbt_get_redux_field('introduction_section-title') ?></h2>
                <p>
+                  <svg>
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#emo-smile_1">
+                     </use>
+                  </svg>
+
                   <?php echo rmbt_get_redux_field('introduction_section-text') ?>
                </p>
             </article>
             <article>
                <h2><?php echo rmbt_get_redux_field('introduction_first_block-title') ?></h2>
                <p>
+                  <svg>
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#group_1">
+                     </use>
+                  </svg>
                   <?php echo rmbt_get_redux_field('introduction_first_block-text') ?>
                </p>
             </article>
             <article>
                <h2><?php echo rmbt_get_redux_field('introduction_second_block-title') ?></h2>
                <p>
+                  <svg>
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#worker-cap_1">
+                     </use>
+                  </svg>
+
                   <?php echo rmbt_get_redux_field('introduction_second_block-text') ?>
                </p>
             </article>
          </div>
-         <div class="rmbt-introduction__img-col">
-            <figure class="wrap-img">
-               <?php rmbt_redux_img('introduction_image', rmbt_get_redux_field('introduction_image_alt')) ?>
-            </figure>
-         </div>
+         <figure class="rmbt-introduction__img-col wrap-img">
+            <?php rmbt_redux_img('introduction_image', rmbt_get_redux_field('introduction_image_alt')) ?>
+         </figure>
       </div>
    </section>
 </div>
