78a1f2cade212e45979cb994492992250d4f0cde
Rombt <romron@ukr.net>
2024-04-20 16:12:19 +0300

    html and styles for categories_grouping are ready

--- app/src/core-plugin/assets/js/modules/categories_grouping.js
+++ app/src/core-plugin/assets/js/modules/categories_grouping.js
@@ -1,94 +1,104 @@
 // let arr_categories = [];
 
 async function getAllCategories() {
-  let response = await fetch(ajaxurl + '?action=get_all_categories');
-  const result = await response.json();
-  if (result.success) {
-    return result.data;
-  } else {
-    // here is the handling of the situation when there are no categories
-  }
+    let response = await fetch(ajaxurl + '?action=get_all_categories');
+    const result = await response.json();
+    if (result.success) {
+        return result.data;
+    } else {
+        // here is the handling of the situation when there are no categories
+    }
 }
 
-window.onload = async function () {
-  arr_categories = await getAllCategories();
-  const mainWrapPage = document.querySelector('.rmbt-categories-grouping-wrap');
-  const blocksPage = {
-    wrapDisplayCategories: document.createElement('div'),
-    category: {
-      wrapCategory: document.createElement('div'),
-      bodyCategory: document.createElement('div'),
-      quantityProducts: document.createElement('div'),
-      titleCategory: document.createElement('h3'),
-      descriptionCategory: document.createElement('p'),
-      addToGroup: document.createElement('button'),
-    },
-    wrapGroupsCategories: document.createElement('div'),
-    group: {
-      wrapGroup: document.createElement('div'),
-      bodyGroup: document.createElement('div'),
-      textGroup: document.createElement('div'),
-      inputGroup: document.createElement('div'),
-      categoriesField: document.createElement('div'),
-      titleGroup: document.createElement('lible'),
-      titleGroupInput: document.createElement('input'),
-      descriptionGroup: document.createElement('textarea'),
-      publishGroup: document.createElement('button'),
-    },
-  };
+window.onload = async function() {
+    arr_categories = await getAllCategories();
+    const mainWrapPage = document.querySelector('.rmbt-categories-grouping-wrap');
+    const blocksPage = {
+        wrapDisplayCategories: document.createElement('div'),
+        category: {
+            wrapCategory: document.createElement('div'),
+            bodyCategory: document.createElement('div'),
+            quantityProducts: document.createElement('div'),
+            titleCategory: document.createElement('h3'),
+            descriptionCategory: document.createElement('p'),
+            addToGroup: document.createElement('button'),
+        },
+        wrapGroupsCategories: document.createElement('div'),
+        group: {
+            wrapGroup: document.createElement('div'),
+            bodyGroup: document.createElement('div'),
+            bodyGroupName: document.createElement('div'),
+            bodyGroupDescription: document.createElement('div'),
+            controlsGroup: document.createElement('div'),
+            publishGroup: document.createElement('button'),
+            deleteGroup: document.createElement('button'),
+            bodyGroupPGroupName: document.createElement('p'), //todo в имя свойства добавить ключевое слово Tag и в genCssClassName(str) и в addClassToBlocks(objBlocks) пропускать имена свойств с этим ключём
+            bodyGroupInputGroupName: document.createElement('input'), //todo в имя свойства добавить ключевое слово Tag и в genCssClassName(str) и в addClassToBlocks(objBlocks) пропускать имена свойств с этим ключём
+            bodyGroupPGroupDescription: document.createElement('p'), //todo в имя свойства добавить ключевое слово Tag и в genCssClassName(str) и в addClassToBlocks(objBlocks) пропускать имена свойств с этим ключём
+            bodyGroupInputGroupDescription: document.createElement('textarea'), //todo в имя свойства добавить ключевое слово Tag и в genCssClassName(str) и в addClassToBlocks(objBlocks) пропускать имена свойств с этим ключём
+            categoriesField: document.createElement('div'),
 
-  addClassToBlocks(blocksPage);
 
-  blocksPage.category.addToGroup.textContent = 'add to group';
-  blocksPage.category.bodyCategory.append(blocksPage.category.titleCategory);
-  blocksPage.category.wrapCategory.append(blocksPage.category.bodyCategory);
-  blocksPage.category.bodyCategory.append(blocksPage.category.descriptionCategory);
-  blocksPage.category.wrapCategory.append(blocksPage.category.quantityProducts);
-  blocksPage.category.wrapCategory.append(blocksPage.category.addToGroup);
+        },
+    };
 
-  arr_categories.forEach(objCategory => {
-    // console.log('objCategory = ', objCategory);
-    let currentCat = blocksPage.category.wrapCategory.cloneNode(true);
-    currentCat.querySelector('.title-category').textContent = objCategory.cat_name;
-    currentCat.querySelector('.description-category').textContent = objCategory.category_description;
-    currentCat.querySelector('.quantity-products').textContent = objCategory.count;
-    blocksPage.wrapDisplayCategories.append(currentCat);
-  });
+    addClassToBlocks(blocksPage);
 
-  blocksPage.group.titleGroup.textContent = 'Input name your group';
-  blocksPage.group.descriptionGroup.textContent = 'Input description your group';
-  blocksPage.group.publishGroup.textContent = 'publish this group';
+    blocksPage.category.addToGroup.textContent = 'add to group';
+    blocksPage.category.bodyCategory.append(blocksPage.category.titleCategory);
+    blocksPage.category.wrapCategory.append(blocksPage.category.bodyCategory);
+    blocksPage.category.bodyCategory.append(blocksPage.category.descriptionCategory);
+    blocksPage.category.wrapCategory.append(blocksPage.category.quantityProducts);
+    blocksPage.category.wrapCategory.append(blocksPage.category.addToGroup);
 
-  blocksPage.group.textGroup.append(blocksPage.group.titleGroup);
-  blocksPage.group.textGroup.append(blocksPage.group.descriptionGroup);
-  blocksPage.group.bodyGroup.append(blocksPage.group.textGroup);
+    arr_categories.forEach(objCategory => {
+        // console.log('objCategory = ', objCategory);
+        let currentCat = blocksPage.category.wrapCategory.cloneNode(true);
+        currentCat.querySelector('.title-category').textContent = objCategory.cat_name;
+        currentCat.querySelector('.description-category').textContent = objCategory.category_description;
+        currentCat.querySelector('.quantity-products').textContent = objCategory.count;
+        blocksPage.wrapDisplayCategories.append(currentCat);
+    });
 
-  blocksPage.group.inputGroup.append(blocksPage.group.titleGroupInput);
-  blocksPage.group.inputGroup.append(blocksPage.group.publishGroup);
-  blocksPage.group.bodyGroup.append(blocksPage.group.inputGroup);
+    blocksPage.group.bodyGroupPGroupName.textContent = 'Enter group name';
+    blocksPage.group.bodyGroupPGroupDescription.textContent = 'Enter group description';
+    blocksPage.group.publishGroup.textContent = 'publish this group';
+    blocksPage.group.deleteGroup.textContent = 'delete this group';
 
-  blocksPage.group.wrapGroup.append(blocksPage.group.bodyGroup);
-  blocksPage.group.wrapGroup.append(blocksPage.group.categoriesField);
-  blocksPage.wrapGroupsCategories.append(blocksPage.group.wrapGroup);
+    blocksPage.group.bodyGroupName.append(blocksPage.group.bodyGroupPGroupName);
+    blocksPage.group.bodyGroupName.append(blocksPage.group.bodyGroupInputGroupName);
+    blocksPage.group.bodyGroupDescription.append(blocksPage.group.bodyGroupPGroupDescription);
+    blocksPage.group.bodyGroupDescription.append(blocksPage.group.bodyGroupInputGroupDescription);
+    blocksPage.group.bodyGroup.append(blocksPage.group.bodyGroupName);
+    blocksPage.group.bodyGroup.append(blocksPage.group.bodyGroupDescription);
 
-  mainWrapPage.append(blocksPage.wrapGroupsCategories);
-  mainWrapPage.append(blocksPage.wrapDisplayCategories);
+    blocksPage.group.controlsGroup.append(blocksPage.group.deleteGroup);
+    blocksPage.group.controlsGroup.append(blocksPage.group.publishGroup);
+    blocksPage.group.bodyGroup.append(blocksPage.group.controlsGroup);
+
+
+    blocksPage.group.wrapGroup.append(blocksPage.group.bodyGroup);
+    blocksPage.group.wrapGroup.append(blocksPage.group.categoriesField);
+    blocksPage.wrapGroupsCategories.append(blocksPage.group.wrapGroup);
+
+    mainWrapPage.append(blocksPage.wrapGroupsCategories);
+    mainWrapPage.append(blocksPage.wrapDisplayCategories);
 };
 
 function addClassToBlocks(objBlocks) {
-  Object.entries(objBlocks).forEach(([name, node]) => {
-    processNode(name, node);
-  });
+    Object.entries(objBlocks).forEach(([name, node]) => {
+        processNode(name, node);
+    });
 
-  function processNode(name, node) {
-    if (node instanceof Element) {
-      node.classList.add(genCssClassName(name));
-    } else if (typeof node === 'object' && node !== null) {
-      Object.entries(node).forEach(([childName, childNode]) => {
-        processNode(childName, childNode);
-      });
+    function processNode(name, node) {
+        if (node instanceof Element) {
+            node.classList.add(genCssClassName(name));
+        } else if (typeof node === 'object' && node !== null) {
+            Object.entries(node).forEach(([childName, childNode]) => {
+                processNode(childName, childNode);
+            });
+        }
     }
-  }
 }
 
 // -------------  helpers ---------------
@@ -98,18 +108,18 @@ function addClassToBlocks(objBlocks) {
  *
  */
 function genCssClassName(str) {
-  let result = '';
+    let result = '';
 
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    if (char.toUpperCase() === char) {
-      result += `-${char.toLowerCase()}`;
-    } else {
-      result += char;
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        if (char.toUpperCase() === char) {
+            result += `-${char.toLowerCase()}`;
+        } else {
+            result += char;
+        }
     }
-  }
 
-  console.log(result);
+    console.log(result);
 
-  return result;
+    return result;
 }
--- app/src/core-plugin/parts/categories_grouping.php
+++ app/src/core-plugin/parts/categories_grouping.php
@@ -21,8 +21,27 @@ function rmbt_categories_group()
 ?>
    <div class="wrap">
       <h1><?= get_admin_page_title() ?></h1>
+      <button class="page-title-action">Add new group</button>
       <div class="rmbt-categories-grouping-wrap">
-      </div>
+
+<!--          <div class="wrap-groups-categories">
+            <div class="wrap-group">
+               <div class="body-group">
+                  <div class="body-group__name">
+                     <p>Enter group name</p>
+                     <input id="group-name" type="text">
+                  </div>
+                  <div class="body-group__description">
+                     <p>Inter description category</p>
+                     <textarea id="group-description" ></textarea>
+                  </div>
+
+                  <button class="publish-group">publish this group</button>
+               </div>
+               <div class="gategories-field"></div>
+            </div>
+      </div> -->
+
    </div>
 <?php
 }
