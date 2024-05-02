d0d511cd63a6ee488b3d313062f78281d1ff6911
Rombt <Rombt@gmail.com>
2024-05-02 16:45:13 +0300

    ...

--- app/src/core-plugin/assets/js/modules/bakeries_meta_boxes.js
+++ app/src/core-plugin/assets/js/modules/bakeries_meta_boxes.js
@@ -7,8 +7,35 @@ function bakeriesMetaBoxes() {
     return false;
   }
 
-  const technologyCardImg = document.createElement('');
-  const technologyCardImgAddBut = document.createElement('');
+  const technologyCardWrap = document.createElement('div');
+  //   technologyCardWrap.classList.add('');
+
+  const wrapTechnologyCardImg = document.createElement('div'); // содержит всё что касается добавления картинки
+  //   wrapTechnologyCardImg.classList.add('');
+
+  const wrapImgTechnologyCard = document.createElement('div'); // содержит саму картинку
+  wrapImgTechnologyCard.classList.add('wrap-img');
+
+  const technologyCardImg = document.createElement('img');
+
+  const addButTechnologyCardImg = document.createElement('div');
+  //   addButTechnologyCardImg.classList.add('');
+
+  wrapImgTechnologyCard.append(technologyCardImg);
+  wrapTechnologyCardImg.append(wrapImgTechnologyCard);
+  wrapTechnologyCardImg.append(addButTechnologyCardImg);
+
+  const equipmentsWrap = document.createElement('div');
+  const equipment = document.createElement('div');
+  const addButEquipment = document.createElement('button');
+
+  equipmentsWrap.append(equipment);
+  equipmentsWrap.append(addButEquipment);
+
+  technologyCardWrap.append(equipmentsWrap);
+  technologyCardWrap.append(wrapTechnologyCardImg);
+
+  mainWrap.append(technologyCardWrap);
 }
 
 export default bakeriesMetaBoxes;
