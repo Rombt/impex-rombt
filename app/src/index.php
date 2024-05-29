6ff5cc1fd025abb31ce3ac040b99b59469547543
Rombt <Rombt@gmail.com>
2024-05-29 12:38:23 +0300

    mob version of blog page is ready

--- app/src/index.php
+++ app/src/index.php
@@ -3,31 +3,34 @@
 
 
 
-<main class="wrapper-section">
-   <div class="rmbt-full-width rmbt-blog-full-width">
-      <section class="rmbt-container rmbt-blog">
-         <!-- <h1><?php  ?></h1> -->
-         <div class="rmbt-blog__row">
-            <div class="rmbt-blog__col">
-               <?php if (have_posts()) {
-                  while (have_posts()) :
-                     the_post();
+<main>
+   <div class="wrapper-section">
+      <div class="rmbt-full-width rmbt-blog-full-width">
+         <section class="rmbt-container rmbt-blog">
+            <div class="rmbt-blog__row">
+               <div class="rmbt-blog__col">
+                  <?php
+                  if (have_posts()) {
+                     while (have_posts()) :
+                        the_post();
 
-                     get_template_part('template-parts/components/card_news', null, [
-                        'title' => get_the_title(),
-                        'text' => get_the_excerpt(),
-                        'tag-img' => get_the_post_thumbnail(),
-                        'link_read_more_href' => get_the_permalink(),
-                     ]);
+                        // get_template_part('template-parts/components/card_news_blocks', null, [
+                        get_template_part('template-parts/components/card_news_strings', null, [
+                           'title' => get_the_title(),
+                           'text' => get_the_excerpt(),
+                           'tag-img' => get_the_post_thumbnail(),
+                           'link_read_more_href' => get_the_permalink(),
+                        ]);
 
-                  endwhile;
-               } else {
-                  //   get_template_part('partials/notfound');
-               }
-               ?>
+                     endwhile;
+                  } else {
+                     //   get_template_part('partials/notfound');
+                  }
+                  ?>
+               </div>
             </div>
-         </div>
-      </section>
+         </section>
+      </div>
    </div>
 </main>
 
@@ -35,7 +38,8 @@
 
 
 
-<?php get_template_part('template-parts/components/pagination'); ?>
+<?php get_template_part('template-parts/components/pagination');
+?>
 
 
 
---
+++ app/src/template-parts/components/card_news_strings.php
@@ -0,0 +1,17 @@
+<article class="rmbt-card-news-article-string rmbt-shadow">
+   <div class="rmbt-card-news-article-string__body">
+      <div class="wrap-img rmbt-card-news-article-string__img">
+         <?php echo $args['tag-img'] ?>
+      </div>
+      <h3 class="section-title"> <?php echo $args['title'] ?></h3>
+      <?php echo $args['text'] ?>
+      <div class="card-footer">
+         <a href="<?php echo $args['link_read_more_href'] ?>">
+            read more
+            <svg>
+               <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#arrow-right_1"></use>
+            </svg>
+         </a>
+      </div>
+   </div>
+</article>
