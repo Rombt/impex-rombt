06617df5aa16b818e2204a3f4b389985b6108c72
Rombt <Rombt@gmail.com>
2024-06-03 13:59:28 +0300

    search and archive pages is ready

--- app/src/index.php
+++ app/src/index.php
@@ -1,20 +1,29 @@
 <?php get_header(); ?>
 
-
-
-
 <main>
    <div class="wrapper-section">
       <div class="rmbt-full-width rmbt-blog-full-width">
          <section class="rmbt-container rmbt-blog">
             <div class="rmbt-blog__row">
+
                <div class="rmbt-blog__col">
+                  <!-- <header class="page-header "> -->
+                  <h1 class="page-title">
+                     <?php if (is_search()) {
+                        printf(esc_html__('Результати пошуку для: %s', 'rmbt_impex'), '<span>' . get_search_query() . '</span>');
+                     } else if (is_home() || is_page(['blog', 'Blog'])) {
+                        echo "Новини нашої компанії";
+                     } else if (is_archive()) {
+                        the_archive_title();
+                        the_archive_description();
+                     } ?>
+                  </h1>
+                  <!-- </header> -->
                   <?php
                   if (have_posts()) {
                      while (have_posts()) :
                         the_post();
 
-                        // get_template_part('template-parts/components/card_news_blocks', null, [
                         get_template_part('template-parts/components/card_news_strings', null, [
                            'title' => get_the_title(),
                            'text' => get_the_excerpt(),
@@ -35,12 +44,7 @@
 </main>
 
 
-
-
-
-<?php get_template_part('template-parts/components/pagination');
-?>
-
+<?php get_template_part('template-parts/components/pagination'); ?>
 
 
 
