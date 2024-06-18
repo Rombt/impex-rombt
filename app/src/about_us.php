2171579a64b5e63cced9bd5bc6d57911a97b0a9e
Rombt <Rombt@gmail.com>
2024-06-18 10:22:05 +0300

    about_us page is ready

--- app/src/about_us.php
+++ app/src/about_us.php
@@ -6,18 +6,12 @@
 <main>
 
    <div class="wrapper-section">
-      <div class="rmbt-full-width rmbt-contacts-full-width">
-         <section class="rmbt-container rmbt-contacts text-content">
-            <?php get_template_part('template-parts/components/title', 'page', ['title' => get_the_title()]);
-            ?>
-            <!-- <p><?php // echo rmbt_get_redux_field('rmbt-contacts_section-text') 
-                     ?></p> -->
+      <div class="rmbt-full-width rmbt-about-us-full-width">
+         <section class="rmbt-container rmbt-about-us text-content">
+            <?php get_template_part('template-parts/components/title', 'page', ['title' => get_the_title()]); ?>
             <div class="rmbt-about-as__row">
-               <div class="rmbt-contacts__col">
-
+               <div class="rmbt-about-us__col">
                   <?php the_content(); ?>
-
-
                </div>
             </div>
          </section>
