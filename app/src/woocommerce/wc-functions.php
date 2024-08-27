c6df84263cd21ddf3a99b9ffce7b22c02538b6cf
Rombt <rombtnet@gmail.com>
2024-08-27 14:57:00 +0300

    added the size  rmbt_product_main_img
    (cherry picked from commit 9219f88452ed3db83f856f62681b61d3f2f9204a)

--- app/src/woocommerce/wc-functions.php
+++ app/src/woocommerce/wc-functions.php
@@ -1,33 +1,35 @@
 <?php
 
-if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
-   exit;
+if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	exit;
 }
 
-function rmbt_wc_site_setup()
-{
-   add_theme_support('woocommerce');
+function rmbt_wc_site_setup() {
+	add_theme_support( 'woocommerce' );
 }
-add_action('after_setup_theme', 'rmbt_wc_site_setup');
+add_action( 'after_setup_theme', 'rmbt_wc_site_setup' );
 
-add_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+add_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 
 
-function start_wrapper_section_single_product()
-{
-   echo  '<div class="wrapper-section">
+function start_wrapper_section_single_product() {
+	echo '<div class="wrapper-section">
             <div class="rmbt-full-width rmbt-equipment-categories-full-width">
                <section class="rmbt-container rmbt-equipment-categories">';
 }
-add_action('woocommerce_before_main_content', 'start_wrapper_section_single_product', 15);
+add_action( 'woocommerce_before_main_content', 'start_wrapper_section_single_product', 15 );
 
-function end_wrapper_section_single_product()
-{
-   echo '   </section>
+function end_wrapper_section_single_product() {
+	echo '   </section>
          </div>
       </div>';
 }
-add_action('woocommerce_after_main_content hook', 'end_wrapper_section_single_product', 15);
+add_action( 'woocommerce_after_main_content hook', 'end_wrapper_section_single_product', 15 );
 
 
-add_action('woocommerce_before_single_product_summary', 'woocommerce_template_single_title', 5);
+add_action( 'woocommerce_before_single_product_summary', 'woocommerce_template_single_title', 5 );
+
+add_filter( 'woocommerce_gallery_image_size', 'custom_gallery_image_size' );
+function custom_gallery_image_size( $size ) {
+	return 'rmbt_product_main_img';
+}
