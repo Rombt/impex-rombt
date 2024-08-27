ec41287ff3261117e2dd085f7f6e28e5011ff309
Rombt <rombtnet@gmail.com>
2024-08-27 13:14:20 +0300

    ,

--- app/src/template-parts/parts/main_slider_screen.php
+++ app/src/template-parts/parts/main_slider_screen.php
@@ -24,27 +24,27 @@ if ( rmbt_get_redux_field( 'button_href' ) === '' || rmbt_get_redux_field( 'butt
 
 
 <div class="wrapper-section wrapper-section-main-slider-screen">
-   <div class="main-slider-screen rmbt-full-width">
+	<div class="main-slider-screen rmbt-full-width">
 
-      <div class="site-title">
-         <!-- <div class="site-title__slogan"> <?php echo rmbt_get_redux_field( 'front_page_slogan_' . $locale ) ?> </div> -->
-         <div class="site-title__title">
-            <h1><?php echo rmbt_get_redux_field( 'front_page_title_' . $locale, 1 ) ?></h1>
-            <p> <?php echo rmbt_get_redux_field( 'front_page_subtitle_' . $locale ) ?> </p>
-         </div>
-         <?php get_template_part( 'template-parts/components/button-link', null, [ 'href' => $contact_page_url, 'title' => rmbt_get_redux_field( 'button_title_' . $locale ) ] );
+		<div class="site-title">
+			<!-- <div class="site-title__slogan"> <?php echo rmbt_get_redux_field( 'front_page_slogan_' . $locale ) ?> </div> -->
+			<!-- <div class="site-title__title">
+				<h1><?php echo rmbt_get_redux_field( 'front_page_title_' . $locale, 1 ) ?></h1>
+				<p> <?php echo rmbt_get_redux_field( 'front_page_subtitle_' . $locale ) ?> </p>
+			</div> -->
+			<?php get_template_part( 'template-parts/components/button-link', null, [ 'href' => $contact_page_url, 'title' => rmbt_get_redux_field( 'button_title_' . $locale ) ] );
 			?>
-      </div>
+		</div>
 
-      <div class="main-slider-screen__video">
-         <iframe frameborder="0" allowfullscreen=""
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin" title="Impexmash-PROMO"
-            src="https://www.youtube.com/embed/off7w3-tptA?playlist=off7w3-tptA&amp;iv_load_policy=3&amp;enablejsapi=1&amp;disablekb=1&amp;autoplay=1&amp;controls=0&amp;mute=1&amp;showinfo=0&amp;rel=0&amp;loop=1&amp;wmode=transparent&amp;origin=<?php echo get_site_url(); ?>"></iframe>
-      </div>
+		<div class="main-slider-screen__video">
+			<iframe frameborder="0" allowfullscreen=""
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				referrerpolicy="strict-origin-when-cross-origin" title="Impexmash-PROMO"
+				src="https://www.youtube.com/embed/off7w3-tptA?playlist=off7w3-tptA&amp;iv_load_policy=3&amp;enablejsapi=1&amp;disablekb=1&amp;autoplay=1&amp;controls=0&amp;mute=1&amp;showinfo=0&amp;rel=0&amp;loop=1&amp;wmode=transparent&amp;origin=<?php echo get_site_url(); ?>"></iframe>
+		</div>
 
 
 
-   </div>
-   <?php get_template_part( 'template-parts/components/search_block', '3' ); ?>
+	</div>
+	<?php get_template_part( 'template-parts/components/search_block', '3' ); ?>
 </div>
