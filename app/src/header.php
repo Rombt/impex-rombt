19b658377956b80592dc6ab11053f9f751c9acfd
Rombt <rombtnet@gmail.com>
2024-08-30 15:46:51 +0300

    .

--- app/src/header.php
+++ app/src/header.php
@@ -2,45 +2,45 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+   <meta charset="<?php bloginfo( 'charset' ); ?>">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<!-- Google Tag Manager -->
-	<script>
-		(function (w, d, s, l, i) {
-			w[l] = w[l] || [];
-			w[l].push({
-				'gtm.start': new Date().getTime(),
-				event: 'gtm.js'
-			});
-			var f = d.getElementsByTagName(s)[0],
-				j = d.createElement(s),
-				dl = l != 'dataLayer' ? '&l=' + l : '';
-			j.async = true;
-			j.src =
-				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-			f.parentNode.insertBefore(j, f);
-		})(window, document, 'script', 'dataLayer', 'GTM-TPZ85JHZ');
-	</script>
-	<!-- End Google Tag Manager -->
+   <!-- Google Tag Manager -->
+   <script>
+   (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+         'gtm.start': new Date().getTime(),
+         event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+         j = d.createElement(s),
+         dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+         'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+   })(window, document, 'script', 'dataLayer', 'GTM-TPZ85JHZ');
+   </script>
+   <!-- End Google Tag Manager -->
 
-	<?php wp_head(); ?>
+   <?php wp_head(); ?>
 
 </head>
 
 <body <?php body_class(); ?>>
-	<?php wp_body_open(); ?>
-	<?php //  get_template_part('template-parts/components/debug-grid');
+   <?php wp_body_open(); ?>
+   <?php //  get_template_part('template-parts/components/debug-grid');
 	?>
 
-	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPZ85JHZ" height="0" width="0"
-			style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->
+   <!-- Google Tag Manager (noscript) -->
+   <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPZ85JHZ" height="0" width="0"
+         style="display:none;visibility:hidden"></iframe></noscript>
+   <!-- End Google Tag Manager (noscript) -->
 
 
 
-	<div class="rmbt-page-wrap">
+   <div class="rmbt-page-wrap">
 
-		<?php get_template_part( 'template-parts/components/header', '2' ); ?>
+      <?php get_template_part( 'template-parts/components/header', '2' ); ?>
